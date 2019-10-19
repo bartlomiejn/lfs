08f114365936ef27c30197c4f8498bda5fc28c86
Bartlomiej Nowak <bartlomiej.nwk@gmail.com>
2019-10-19 21:25:56 +0200

    Fix g++ compilation test

--- check-deps.sh
+++ check-deps.sh
@@ -43,3 +43,10 @@ sed --version | head -n1
 tar --version | head -n1
 makeinfo --version | head -n1  # texinfo version
 xz --version | head -n1
+echo 'int main(){}' > dummy.c && g++ -o dummy dummy.c
+if [ -x dummy ]
+  then echo "g++ compilation OK";
+else 
+  echo "g++ compilation failed";
+fi
+rm -f dummy.c dummy
